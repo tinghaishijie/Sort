19780e41c5c0743c21262d2d48748f4ec44fa8d5
Zheng <597002248@qq.com>
2017-10-17 17:59:59 +0800

    冒泡

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,4 +1,22 @@
 
 public class BubbleSort {
 
+	public static int[] BubbleSort(int arr[]){
+		for(int i=0;i<arr.length-1;i++){//外层循环控制排序趟数
+			for(int j=0;j<arr.length-1-i;j++){//内层循环控制每一趟排序多少次
+				if(arr[j]>arr[j+1]){
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		} 
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		int[] a = {3,4,2,6,7,1};
+		BubbleSort(a);
+		for(int i:a) System.out.print(i);
+	}
 }
